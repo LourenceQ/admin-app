76ece502b6b3047c79ed91ab532aff0ed55f0fe6
LourenceQ <lawrenceqf@gmail.com>
2023-03-07 21:30:17 -0300

    Implementando inicio de componente Rodape

---
+++ src/components/Rodape/index.tsx
@@ -0,0 +1,17 @@
+import facebook from './assets/facebook.png';
+import whatsapp from './assets/whatsapp.png';
+import coinstagramnsulta from './assets/instagram.png';
+import google from './assets/google.png';
+import styled from 'styled-components';
+
+function Rodape() {
+    return(
+        <footer>
+            <ul>
+                <li>
+                    <a href="#"></a>
+                </li>
+            </ul>
+        </footer>
+    )
+}
