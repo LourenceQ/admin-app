7130de156bb06ed31658cb3d4230914dfdd6fe17
LourenceQ <lawrenceqf@gmail.com>
2023-07-27 21:50:41 -0300

    Finalizando tabela

--- src/components/Tabela/index.tsx
+++ src/components/Tabela/index.tsx
@@ -1,21 +1,31 @@
 import { Paper, Table, TableCell, TableHead, TableRow, TableBody } from "@mui/material";
 import TableContainer from "@mui/material/TableContainer";
+import IConsulta from "../../types/IConsulta";
 
-function Tabela() {
+function Tabela({consultas} : {consultas: IConsulta[] | null}) {
     return (
         <>
             <TableContainer component={Paper}>
                 <Table sx={{minWidth: 700}} aria-label="tabela-customizada">
-                    <TableHead>
+                <TableHead>
                         <TableRow>
                             <TableCell>Data</TableCell>
+                            <TableCell>Horário</TableCell>
+                            <TableCell>Profissional</TableCell>
+                            <TableCell>Especialidade</TableCell>
+                            <TableCell>Paciente</TableCell>
+                            <TableCell>Modalidade</TableCell>
                         </TableRow>
-                    </TableHead>
+                    </TableHead>                    
                     <TableBody>
                         <TableRow>
-                            <TableCell component = "th" scope="row">22/03/2023</TableCell>
-                            <TableCell>20:30</TableCell>
-                        </TableRow>
+                           <TableCell component="th" scope="row">22/02/2023</TableCell>
+                             <TableCell>08:30</TableCell>
+                             <TableCell>Monica Hillman</TableCell>
+                             <TableCell>Clinica Geral</TableCell>
+                             <TableCell>Caroline Carvalho</TableCell>
+                             <TableCell>Particular</TableCell>
+                          </TableRow>
                     </TableBody>
                 </Table>
             </TableContainer>
