0106fba4541617c9c3748e18362c30a3bfc21326
LourenceQ <lawrenceqf@gmail.com>
2023-08-02 20:56:30 -0300

    Implementando correções em endpoints e verificação de length de lista em endpoint

--- src/components/Avaliacao/index.tsx
+++ src/components/Avaliacao/index.tsx
@@ -1,6 +1,7 @@
 import Card from "./Card";
 import IProfissional from "../../types/IProfissional";
 import styled from "styled-components";
+import Botao from "../Botao";
 
 const SecaoCard = styled.div`
     display: flex;
@@ -10,12 +11,20 @@ const SecaoCard = styled.div`
 `
 
 function Avaliacao({profissionais} : {profissionais: IProfissional[] | null}) {
-    return (        
+    return (      
+        <>
         <SecaoCard>
-            {profissionais?.map((profissional) => {
-                return <Card profissional={profissional} />
-            })}
+            {profissionais && profissionais.length 
+                ? profissionais?.map((profissional) => {
+                    return <Card profissional={profissional} />
+                })  
+                : "Não há avaliações para mostrar."}
         </SecaoCard>
+        { profissionais && profissionais.length > 4 
+            ? <Botao>Ver mais</Botao>
+            : ""
+        }
+        </>  
     ) 
 }
 export default Avaliacao;
